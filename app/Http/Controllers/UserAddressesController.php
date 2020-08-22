5c488b6afeec9e49fe8e6e6c0cf2cc8471d0a61e
Yong <freeyongx@163.com>
2020-08-22 09:51:08 +0000

    修改和删除收货地址

--- app/Http/Controllers/UserAddressesController.php
+++ app/Http/Controllers/UserAddressesController.php
@@ -45,4 +45,50 @@ class UserAddressesController extends Controller
 
         return redirect()->route('user_addresses.index');
     }
+
+    /**
+     * 更新地址界面
+     *
+     * @param UserAddress $user_address
+     * @return void
+     */
+    public function edit(UserAddress $user_address)
+    {
+        $this->authorize('own', $user_address);
+
+        return view('user_addresses.create_and_edit', ['address' => $user_address]);
+    }
+
+    /**
+     * 更新地址
+     *
+     * @param UserAddress $user_address
+     * @param UserAddressRequest $request
+     * @return void
+     */
+    public function update(UserAddress $user_address, UserAddressRequest $request)
+    {
+        $this->authorize('own', $user_address);
+
+        $user_address->update($request->only([
+            'province', 'city', 'district', 'address', 'zip', 'contact_name', 'contact_phone'
+        ]));
+
+        return redirect()->route('user_addresses.index');
+    }
+
+    /**
+     * 删除地址
+     *
+     * @param UserAddress $user_address
+     * @return void
+     */
+    public function destroy(UserAddress $user_address)
+    {
+        $this->authorize('own', $user_address);
+        
+        $user_address->delete();
+
+        return [];
+    }
 }
--- resources/views/user_addresses/create_and_edit.blade.php
+++ resources/views/user_addresses/create_and_edit.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', '新增收货地址')
+@section('title', ($address->id ? '修改' : '新增') . '收货地址')
 
 @section('content')
 <div class="row">
@@ -8,7 +8,7 @@
         <div class="card">
             <div class="card-header">
                 <h2 class="text-center">
-                    新增收货地址
+                    {{ $address->id ? '修改' : '新增' }}收货地址
                 </h2>
             </div>
             <div class="card-body">
@@ -26,70 +26,81 @@
                 <!-- 输出后端报错结束 -->
                 <!-- inline-template 代表通过内联方式引入组件 -->
                 <user-addresses-create-and-edit inline-template>
-                <form class="form-horizontal" role="form" action="{{ route('user_addresses.store') }}" method="POST">
-                        {{ csrf_field() }}
-                        <!-- inline-template 代表通过内联方式引入组件 -->
-                        <select-district @change="onDistrictChanged" inline-template>
-                            <div class="form-group row">
-                                <label class="col-form-label col-sm-2 text-md-right">省市区</label>
-                                <div class="col-sm-3">
-                                    <select class="form-control" v-model="provinceId">
-                                        <option value="">选择省</option>
-                                        <option v-for="(name, id) in provinces" :value="id">@{{ name }}</option>
-                                    </select>
-                                </div>
-                                <div class="col-sm-3">
-                                    <select class="form-control" v-model="cityId">
-                                        <option value="">选择市</option>
-                                        <option v-for="(name, id) in cities" :value="id">@{{ name }}</option>
-                                    </select>
-                                </div>
-                                <div class="col-sm-3">
-                                    <select class="form-control" v-model="districtId">
-                                        <option value="">选择区</option>
-                                        <option v-for="(name, id) in districts" :value="id">@{{ name }}</option>
-                                    </select>
-                                </div>
-                            </div>
-                        </select-district>
-                        <input type="hidden" name="province" v-model="province">
-                        <input type="hidden" name="city" v-model="city">
-                        <input type="hidden" name="district" v-model="district">
 
-                        <div class="form-group row">
-                            <label class="col-form-label text-md-right col-sm-2">详细地址</label>
-                            <div class="col-sm-9">
-                                <input type="text" class="form-control" name="address"
-                                    value="{{ old('address', $address->address) }}">
+                    @if ($address->id)
+                    <form class="form-horizontal" role="form"
+                        action="{{ route('user_addresses.update', ['user_address' => $address->id]) }}" method="POST">
+                        {{ method_field('PUT') }}
+                    @else
+                        <form class="form-horizontal" role="form" action="{{ route('user_addresses.store') }}"
+                            method="POST">
+                    @endif
+
+                            {{ csrf_field() }}
+                            <!-- inline-template 代表通过内联方式引入组件 -->
+                            <select-district
+                                :init-value="{{ json_encode([old('province', $address->province), old('city', $address->city), old('district', $address->district)]) }}"
+                                @change="onDistrictChanged" inline-template>
+                                <div class="form-group row">
+                                    <label class="col-form-label col-sm-2 text-md-right">省市区</label>
+                                    <div class="col-sm-3">
+                                        <select class="form-control" v-model="provinceId">
+                                            <option value="">选择省</option>
+                                            <option v-for="(name, id) in provinces" :value="id">@{{ name }}</option>
+                                        </select>
+                                    </div>
+                                    <div class="col-sm-3">
+                                        <select class="form-control" v-model="cityId">
+                                            <option value="">选择市</option>
+                                            <option v-for="(name, id) in cities" :value="id">@{{ name }}</option>
+                                        </select>
+                                    </div>
+                                    <div class="col-sm-3">
+                                        <select class="form-control" v-model="districtId">
+                                            <option value="">选择区</option>
+                                            <option v-for="(name, id) in districts" :value="id">@{{ name }}</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </select-district>
+                            <input type="hidden" name="province" v-model="province">
+                            <input type="hidden" name="city" v-model="city">
+                            <input type="hidden" name="district" v-model="district">
+
+                            <div class="form-group row">
+                                <label class="col-form-label text-md-right col-sm-2">详细地址</label>
+                                <div class="col-sm-9">
+                                    <input type="text" class="form-control" name="address"
+                                        value="{{ old('address', $address->address) }}">
+                                </div>
                             </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-form-label text-md-right col-sm-2">邮编</label>
-                            <div class="col-sm-9">
-                                <input type="text" class="form-control" name="zip"
-                                    value="{{ old('zip', $address->zip) }}">
+                            <div class="form-group row">
+                                <label class="col-form-label text-md-right col-sm-2">邮编</label>
+                                <div class="col-sm-9">
+                                    <input type="text" class="form-control" name="zip"
+                                        value="{{ old('zip', $address->zip) }}">
+                                </div>
                             </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-form-label text-md-right col-sm-2">姓名</label>
-                            <div class="col-sm-9">
-                                <input type="text" class="form-control" name="contact_name"
-                                    value="{{ old('contact_name', $address->contact_name) }}">
+                            <div class="form-group row">
+                                <label class="col-form-label text-md-right col-sm-2">姓名</label>
+                                <div class="col-sm-9">
+                                    <input type="text" class="form-control" name="contact_name"
+                                        value="{{ old('contact_name', $address->contact_name) }}">
+                                </div>
                             </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-form-label text-md-right col-sm-2">电话</label>
-                            <div class="col-sm-9">
-                                <input type="text" class="form-control" name="contact_phone"
-                                    value="{{ old('contact_phone', $address->contact_phone) }}">
+                            <div class="form-group row">
+                                <label class="col-form-label text-md-right col-sm-2">电话</label>
+                                <div class="col-sm-9">
+                                    <input type="text" class="form-control" name="contact_phone"
+                                        value="{{ old('contact_phone', $address->contact_phone) }}">
+                                </div>
                             </div>
-                        </div>
-                        <div class="form-group row text-center">
-                            <div class="col-12">
-                                <button type="submit" class="btn btn-primary">提交</button>
+                            <div class="form-group row text-center">
+                                <div class="col-12">
+                                    <button type="submit" class="btn btn-primary">提交</button>
+                                </div>
                             </div>
-                        </div>
-                    </form>
+                        </form>
                 </user-addresses-create-and-edit>
             </div>
         </div>
