5c1a414baf54a3d39842d31d8f38d52a566d0efa
Yong <freeyongx@163.com>
2020-10-06 11:03:42 +0000

    商品后台添加类目字段

--- app/Admin/Controllers/CategoriesController.php
+++ app/Admin/Controllers/CategoriesController.php
@@ -85,12 +85,18 @@ class CategoriesController extends AdminController
         return $form;
     }
 
+    /**
+     * 类目列表接口
+     *
+     * @param Request $request
+     * @return void
+     */
     public function apiIndex(Request $request)
     {
         $search = $request->input('q');
 
         $result = Category::query()
-            ->where('is_directory', true)
+            ->where('is_directory', boolval($request->input('is_directory', true)))
             ->where('name', 'like', '%' . $search . '%')
             ->paginate();
 
--- app/Admin/Controllers/ProductsController.php
+++ app/Admin/Controllers/ProductsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Admin\Controllers;
 
+use App\Models\Category;
 use App\Models\Product;
 use Encore\Admin\Controllers\AdminController;
 use Encore\Admin\Form;
@@ -28,10 +29,11 @@ class ProductsController extends AdminController
 
         $grid->id('ID')->sortable();
         $grid->title('商品名称');
+        $grid->column('category.name', '类目');
         $grid->on_sale('已上架')->display(function ($value) {
             return $value ? '是' : '否';
         });
-        
+
         $grid->price('价格');
         $grid->rating('评分');
         $grid->sold_count('销量');
@@ -61,30 +63,39 @@ class ProductsController extends AdminController
     {
         $form = new Form(new Product());
 
-         // 创建一个输入框，第一个参数 title 是模型的字段名，第二个参数是该字段描述
-         $form->text('title', '商品名称')->rules('required');
+        // 创建一个输入框，第一个参数 title 是模型的字段名，第二个参数是该字段描述
+        $form->text('title', '商品名称')->rules('required');
 
-         // 创建一个选择图片的框
-         $form->image('image', '封面图片')->rules('required|image');
+        // 添加一个商品类目
+        $form->select('category_id', '类目')->options(function ($id) {
+            $category = Category::find($id);
 
-         // 创建一个富文本编辑器
-         $form->quill('description', '商品描述')->rules('required');
+            if ($category) {
+                return [$category->id => $category->full_name];
+            }
+        })->ajax('/admin/api/categories?is_directory=0');
 
-         // 创建一组单选框
-         $form->radio('on_sale', '上架')->options(['1' => '是', '0' => '否'])->default(0);
+        // 创建一个选择图片的框
+        $form->image('image', '封面图片')->rules('required|image');
 
-         // 直接添加一对多的关联模型
-         $form->hasMany('skus', 'SKU 列表', function (Form\NestedForm $form) {
+        // 创建一个富文本编辑器
+        $form->quill('description', '商品描述')->rules('required');
+
+        // 创建一组单选框
+        $form->radio('on_sale', '上架')->options(['1' => '是', '0' => '否'])->default(0);
+
+        // 直接添加一对多的关联模型
+        $form->hasMany('skus', 'SKU 列表', function (Form\NestedForm $form) {
             $form->text('title', 'SKU 名称')->rules('required');
             $form->text('description', 'SKU 描述')->rules('required');
             $form->text('price', '单价')->rules('required|numeric|min:0.01');
             $form->text('stock', '剩余库存')->rules('required|integer|min:0');
-         });
+        });
 
-         // 定义事件回调，当模型即将保存时会触发这个回调
-         $form->saving(function (Form $form) {
+        // 定义事件回调，当模型即将保存时会触发这个回调
+        $form->saving(function (Form $form) {
             $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)->min('price') ?: 0;
-         });
+        });
 
         return $form;
     }
