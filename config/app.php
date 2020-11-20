af549759f9d6b542bf6c4613e9b5c9b4d2a04199
Yong <freeyongx@163.com>
2020-11-20 01:27:50 +0000

    创建分期付款

--- config/app.php
+++ config/app.php
@@ -234,4 +234,14 @@ return [
      * 订单失效时间
      */
     'order_ttl' => 1800,
+    // 分期费率，key 为期数，value 为费率
+    'installment_fee_rate' => [
+        3 => 1.5,
+        6 => 2,
+        12 => 2.5,
+    ],
+    // 最低分期金额
+    'min_installment_amount' => 300,
+    // 逾期日息 0.05%
+    'installment_fine_rate' => 0.05,
 ];
